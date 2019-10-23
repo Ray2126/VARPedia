6b533061c3ac02c1e35a18fe9a658ac43850697a
Raymond Yang <50732795+Ray2126@users.noreply.github.com>
2019-10-23 16:59:32 +1300

    Added icon for volume button in video player (#56)

--- src/varpedia/components/videoPlayer/VolumeControl.java
+++ src/varpedia/components/videoPlayer/VolumeControl.java
@@ -1,13 +1,21 @@
 package varpedia.components.videoPlayer;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.media.MediaPlayer;
 
@@ -19,7 +27,8 @@ public class VolumeControl extends HBox{
 	private double _previousVolume;
 
 	public VolumeControl() {
-		_muteButton = new Button("Mute");
+		_muteButton = new Button();
+		volumeUpIcon();
 		_muteButton.setMaxWidth(60);
 		_muteButton.setMinWidth(60);
 		_muteButton.setDisable(true);
@@ -42,20 +51,20 @@ public class VolumeControl extends HBox{
 			@Override
 			public void handle(ActionEvent arg0) {
 				if(_mediaPlayer.getVolume() == 0){
-					_muteButton.setText("Muted");
+					volumeOffIcon();
 					if(_previousVolume == -1) {
 						_mediaPlayer.setVolume(0);
 					}
 					else {
 						_mediaPlayer.setVolume(_previousVolume);
 						_previousVolume = -1;
-						_muteButton.setText("Mute");
+						volumeUpIcon();
 					}
 				}
 				else {
 					_previousVolume = _mediaPlayer.getVolume();
 					_mediaPlayer.setVolume(0);
-					_muteButton.setText("Muted");
+					volumeOffIcon();
 				}
 				
 				
@@ -85,12 +94,36 @@ public class VolumeControl extends HBox{
 			@Override
 			public void changed(ObservableValue<? extends Number> arg0, Number arg1, Number arg2) {
 				if(_volumeSlider.getValue() == 0) {
-					_muteButton.setText("Muted");
+					volumeOffIcon();
 				}
 				else {
-					_muteButton.setText("Mute");
+					volumeUpIcon();
 				}
 			}
 		});
+	}
+	
+	private void volumeUpIcon() {
+		try {
+			BufferedImage image = ImageIO.read(new File("resources/icons/volumeUp.png"));
+			ImageView imageView = new ImageView(SwingFXUtils.toFXImage(image, null));
+			imageView.fitHeightProperty().set(30);
+			imageView.fitWidthProperty().set(30);
+			_muteButton.setGraphic(imageView);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void volumeOffIcon() {
+		try {
+			BufferedImage image = ImageIO.read(new File("resources/icons/volumeOff.png"));
+			ImageView imageView = new ImageView(SwingFXUtils.toFXImage(image, null));
+			imageView.fitHeightProperty().set(30);
+			imageView.fitWidthProperty().set(30);
+			_muteButton.setGraphic(imageView);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
